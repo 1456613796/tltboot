57d45035cc793d21076c29d98d7eb8dfcf1c6f70
1456613796 <1456613796@qq.com>
2019-03-05 13:07:19 +0800

    添加新的类

--- src/main/java/com/gg/tlt/controller/controler.java
+++ src/main/java/com/gg/tlt/controller/controler.java
@@ -1,7 +1,8 @@
 package com.gg.tlt.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping()
+@Controller
 public class controler {
 }
